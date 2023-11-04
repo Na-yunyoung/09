2b3e228919475e7d853eb384287df6fc8a177de4
Na-yunyoung <yunyoung23@sookmyung.ac.kr>
2023-11-05 03:18:11 +0900

    09 2-1

--- main.c
+++ main.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define N_student  5
+#define N_student 5
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int grade[N_student];
-	
 	int i;
+	int grade[N_student];
+	printf("input 5 scores : ");
 	
-	for (i=0;i<N_student;i++)
-		grade[i] = (i+1)*10;
+	for(i=0;i<N_student;i++)
+		scanf("%d",&grade[i]);
 	
-	for (i=0;i<N_student;i++)
-		printf("grade[%i] = %i\n",i,grade[i]);
+	for(i=0;i<N_student;i++)
+		printf("grade[%d] = %d\n",i,grade[i]);
+	
+	
 	
 	return 0;
 }
